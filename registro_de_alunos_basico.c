9743fd99b025458f67266233c680465f21184bd4
Leandro Oliveira Spatafora <leandro.spatafora@hotmail.com>
2025-02-25 02:05:12 -0300

    Atualizar o

---
+++ registro_de_alunos_basico.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+    
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    
+    printf("Sigite seu nome: \n");
+    scanf("%s", nome);
+    
+    printf("Digite o número da sua matrícula: \n");
+    scanf("%d", &matricula);
+    
+    printf("Nome do aluno: %s - matrícula: %d \n", nome, matricula);
+    printf("Idade: %d - Altura: %.2f", idade, altura);
+    return 0;
+}
