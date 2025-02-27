dc807d0527462179d9ccd8b433e668e6afed2b49
Leandro Oliveira Spatafora <leandro.spatafora@hotmail.com>
2025-02-27 02:13:32 -0300

    Atualizar o SuperTrunfo_01_cadastro_cartas.c
    Calcula e fornece densidade populacional e pib per capita

--- SuperTrunfo_01_cadastro_cartas.c
+++ SuperTrunfo_01_cadastro_cartas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    
+
     //Declarando variáveis, para armazenar dados de carta 1 e carta 2
     char estado1, estado2;
     char codigo1[4], codigo2[4];
@@ -10,25 +10,32 @@ int main(){
     float area1, area2, pib1, pib2;
     int pontos1, pontos2;
     
+    //Declarando variáveis, para armazenar dados de densidade e pib per capita de carta 1 e carta 2
+    float densidade1, densidade2, pibpercap1, pibpercap2;
+
     //Solicita dados da primeira carta para o usuário
     printf("Qual Estado da 1° carta? (Letra entre 'A' à 'H'): ");
     scanf("%c", &estado1);
     getchar(); //Getchar remove \n deixado pelo scanf
     printf("Digite o código da carta: ");
     scanf("%s", &codigo1);
-    getchar(); 
+    getchar();
     printf("Digite o nome da cidade: ");
     fgets(nome1, 170, stdin); //fgets consegue armazenar strings com espaços
     nome1[strcspn(nome1, "\n")] = 0; //remove o "\n" deixado pelo fgets
     printf("Qual a população dessa cidade?: ");
     scanf("%d", &populacao1);
-    printf("Qaul a área da cidade em quilômetros quadrados?: ");
+    printf("Qual a área da cidade em quilômetros quadrados?: ");
     scanf("%f", &area1);
-    printf("Qual o PIB da cidade?: ");
+    printf("Qual o PIB da cidade? (em bilhões): ");
     scanf("%f", &pib1);
-    printf("Por final, quantos pontos turísticos?: ");
+    printf("Por fim, quantos pontos turísticos?: ");
     scanf("%d", &pontos1);
     
+    //Calculando e armazena valores de densidade e pib per capita da carta 1
+    densidade1 = (float) populacao1 / area1;
+    pibpercap1 = (pib1 * 1e9) / (float) populacao1; //'(pib1 * 1e9)' == pib1 * 1.000.000.000 (1 bilhão)
+
     //Solicita dados da segunda carta para o usuário
     getchar();
     printf("\nQual Estado da 2° carta? (Letra entre 'A' à 'H'): ");
@@ -36,19 +43,24 @@ int main(){
     getchar();
     printf("Digite o código da carta: ");
     scanf("%s", &codigo2);
-    getchar(); 
+    getchar();
     printf("Digite o nome da cidade: ");
     fgets(nome2, 170, stdin);
     nome2[strcspn(nome2, "\n")] = 0;
     printf("Qual a população dessa cidade?: ");
     scanf("%d", &populacao2);
-    printf("Qaul a área da cidade em quilômetros quadrados?: ");
+    printf("Qual a área da cidade em quilômetros quadrados?: ");
     scanf("%f", &area2);
-    printf("Qual o PIB da cidade?: ");
+    printf("Qual o PIB da cidade? (em bilhões): ");
     scanf("%f", &pib2);
     printf("Por fim, quantos pontos turísticos?: ");
     scanf("%d", &pontos2);
     
+    //Calcula e armazena valores de densidade e pib per capita da carta 2
+    densidade2 = (float) populacao2 / area2;
+    pibpercap2 = (pib2 * 1e9) / (float) populacao2; 
+    
+
     //Exibe informações cadastradas da carta 1
     printf("\nCarta 1: \n");
     printf("Estado: %c\n", estado1);
@@ -58,7 +70,9 @@ int main(){
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de pontos turísticos: %d\n", pontos1);
-    
+    printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibpercap1);
+
     //Exibe informações cadastradas da carta 2
     printf("\nCarta 2: \n");
     printf("Estado: %c\n", estado2);
@@ -68,6 +82,8 @@ int main(){
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de pontos turísticos: %d\n", pontos2);
-    
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per capita: %.2f reais\n", pibpercap2);
+
     return 0;
 }
