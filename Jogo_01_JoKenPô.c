dd1fb281fa8d370786fa8091e3226afa22109037
Leandro Oliveira Spatafora <leandro.spatafora@hotmail.com>
2025-03-13 23:06:52 -0300

    Atualizar o Jogo_01_JoKenPô.c

--- Jogo_01_JoKenPô.c
+++ Jogo_01_JoKenPô.c
@@ -16,10 +16,12 @@ int main(void){
     printf("\nDigite aqui: ");
     scanf("%hd", &TelaInicial);
     
+    //Executa o jogo, exibe as regras ou emite mensagem de erro para usuário
     switch(TelaInicial){
     
         case 1:
-            printf("Veremos quem vai vencer!!!\n");
+            //Menu de opções para usuário (Player1) fazer escolha
+            printf("\nVeremos quem vai vencer!!!\n");
             printf("\n////////////////////////////\n");
             printf("Digite 1, para escolher Pedra\n");
             printf("Digite 2, para escolher Papel\n");
@@ -28,41 +30,40 @@ int main(void){
             printf("\nDigite aqui sua escolha: ");
             scanf("%hd", &Player1);
             
+            //Atribuindo valor aleatório entre 1 e 3 para  computador (Player2)
             srand(time(0));
             Player2 = 1 + rand() % 3;
             
+            //Exibindo escolha do computador(Player2)
+            printf("O computador escolheu... ");
+            switch(Player2){
+                case 1: printf("Pedra!\n"); break;
+                case 2: printf("Papel!\n"); break;
+                case 3: printf("Tesoura!\n"); break;
+            }
+            
+            //Logica para exibir vencedor ou perdedor
             if(Player1 == 1 && Player2 == 3){
-                printf("Você venceu! Pedra vence Tesoura!\n");
+                printf("\nVocê venceu! Pedra vence Tesoura!\n");
             } else if(Player1 == 2 && Player2 == 1){
-                printf("Você venceu! Papel vence Pedra!\n");
+                printf("\nVocê venceu! Papel vence Pedra!\n");
             } else if(Player1 == 3 && Player2 == 2){
-                printf("Você venceu! Tesoura vence Papel!\n");
+                printf("\nVocê venceu! Tesoura vence Papel!\n");
             } else if(Player1 == 1 && Player2 == 2){
-                printf("Xiiii perdeu! Papel vence Pedra!\n");
+                printf("\nXiiii perdeu! Papel vence Pedra!\n");
             } else if(Player1 == 2 && Player2 == 3){
-                printf("Xiiii perdeu! Tesoura vence Papel!\n");
+                printf("\nXiiii perdeu! Tesoura vence Papel!\n");
             } else if(Player1 == 3 && Player2 == 1){
-                printf("Xiiii perdeu! Pedra vence Tesoura!\n");
+                printf("\nXiiii perdeu! Pedra vence Tesoura!\n");
             } else if(Player1 == Player2){
-                printf("Uau! Deu empate!\n");
-                switch(Player1){
-                    case 1:
-                        printf("Ambos escolheram Pedra!\n");
-                        break;
-                    case 2:
-                        printf("Ambos escolheram Papel!\n");
-                        break;
-                    case 3:
-                        printf("Ambos escolheram Tesoura!\n");
-                        break;
-                }
+                printf("\nUau! Deu empate!\n");
             } else {
                 printf("\nErro, selecione uma opção válida!\n");
             }
             break;
             
         case 2:
-            printf("As regras são simples! Veja:\n");
+            printf("\nAs regras são simples! Veja:\n");
             printf("\n- Papel ganha de pedra\n");
             printf("- Pedra ganha de tesoura\n");
             printf("- Tesoura ganha de papel\n");
